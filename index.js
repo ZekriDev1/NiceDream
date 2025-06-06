e1f33d35a85f9d80ad8e46f4235b3a8d7d2e4967
Zekri Akram <akramzekri10@gmail.com>
2025-06-06 16:32:27 +0100

    push new

---
+++ index.js
@@ -0,0 +1,22 @@
+import Jsonfile from "jsonfile";
+import moment from "moment";
+import simpleGit from "simple-git";
+
+const path = "./data.json";
+const date = moment().format();
+
+const data = {
+    date: date,
+};
+
+Jsonfile.writeFile(path, data);
+
+simpleGit().add([path]).commit(date, {'--date' :date }).push();
+
+
+
+
+
+
+
+
